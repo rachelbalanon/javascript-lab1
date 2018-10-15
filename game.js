070ef246379a5a14e745797773d3c5608fe31b98
rachel <rachel.balanon@gmail.com>
2018-10-15 17:10:35 -0400

    initial commit

---
+++ game.js
@@ -0,0 +1,35 @@
+"use strict";
+
+let userName = prompt("What is your name?");
+let grantHealth = 10;
+let userHealth = 40;
+let userWins = 0;
+let play = prompt(`Hi ${userName}! Would you like to play?`); 
+
+if (play === "no" || "No"){
+    console.log(`FINE ${userName}. Nobody wanted to play with you anyway.`);
+    break;
+}
+
+else if (play === "yes" || "Yes"){
+
+    while (play) {
+        userHealth -= Math.floor(Math.random() * 2) + 1;
+        grantHealth -= Math.floor(Math.random() * 2) +1;
+        console.log(`${userName}'s health is ${userHealth}.`)
+        console.log(`Grant's health is ${grantHealth}.`)
+
+        if(grantHealth <= 0) {
+            console.log(`Grant is defeated. ${userName} gains 1 win!`);
+            userWins += 1;
+            grantHealth = 10;
+        } else if (userHealth <= 0) {
+            console.log(`${userName} LOSES!`);
+            break;
+        }
+        else if (userWins >= 3) {
+            console.log(`${userName} won 3 times!`)
+            break;
+        }
+    }
+} 
